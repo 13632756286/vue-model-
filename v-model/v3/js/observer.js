fb0a3fa1c8ab8890fd231b811f0748cac76ce53a
Rechard <1044136908@qq.com>
2020-05-28 22:07:46 +0800

    v-mode为仿写的简易代码

---
+++ v-model/v3/js/observer.js
@@ -0,0 +1,57 @@
+// 1.实现一个监听器Observer，用来劫持并监听所有属性，如果有变动的，就通知订阅者
+function Observer(data) {
+    this.data = data;
+    this.walk(data);
+}
+
+Observer.prototype = {
+    walk: function(data) {
+        var self = this;
+        Object.keys(data).forEach(function(key) {
+            self.defineReactive(data, key, data[key]);
+        });
+    },
+    defineReactive: function(data, key, val) {
+        var dep = new Dep();
+        var childObj = observe(val);
+        Object.defineProperty(data, key, {
+            enumerable: true,
+            configurable: true,
+            get: function getter () {
+                if (Dep.target) {
+                    dep.addSub(Dep.target);
+                }
+                return val;
+            },
+            set: function setter (newVal) {
+                if (newVal === val) {
+                    return;
+                }
+                val = newVal;
+                dep.notify();
+            }
+        });
+    }
+};
+
+function observe(value, vm) {
+    if (!value || typeof value !== 'object') {
+        return;
+    }
+    return new Observer(value);
+};
+
+function Dep () {
+    this.subs = [];
+}
+Dep.prototype = {
+    addSub: function(sub) {
+        this.subs.push(sub);
+    },
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            sub.update();
+        });
+    }
+};
+Dep.target = null;
